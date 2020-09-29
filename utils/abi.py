c4709ea154e2663f81dc11afe4f8628c950eca9b
William Jones <william.jones@physics.ox.ac.uk>
2020-09-29 18:51:40 +0100

    Reduce data fusion notebook to use preprocessed data

--- utils/abi.py
+++ utils/abi.py
@@ -63,3 +63,32 @@ def get_abi_da(dataset, check=False, dtype=None):
     dataarray.attrs['band_id'] = dataset.band_id
     dataarray.attrs['band_wavelength'] = dataset.band_wavelength
     return dataarray
+
+def _contrast_correction(color, contrast):
+    """
+    Modify the contrast of an R, G, or B color channel
+    See: #www.dfstudios.co.uk/articles/programming/image-programming-algorithms/image-processing-algorithms-part-5-contrast-adjustment/
+    Input:
+        C - contrast level
+    """
+    F = (259*(contrast + 255))/(255.*259-contrast)
+    COLOR = F*(color-.5)+.5
+    COLOR = np.minimum(COLOR, 1)
+    COLOR = np.maximum(COLOR, 0)
+    return COLOR
+
+def get_abi_rgb(C01, C02, C03, gamma=0.4, contrast=100):
+    R = np.maximum(C02, 0)
+    R = np.minimum(R, 1)
+    G = np.maximum(C03, 0)
+    G = np.minimum(G, 1)
+    B = np.maximum(C01, 0)
+    B = np.minimum(B, 1)
+    R = np.power(R, gamma)
+    G = np.power(G, gamma)
+    B = np.power(B, gamma)
+    G_true = 0.48358168 * R + 0.45706946 * B + 0.06038137 * G
+    G_true = np.maximum(G_true, 0)
+    G_true = np.minimum(G_true, 1)
+    RGB = _contrast_correction(np.stack([R, G_true, B], -1), contrast=contrast)
+    return RGB
