443541a6f852067e83016e3add3dd00ab13a373b
William Jones <william.jones@physics.ox.ac.uk>
2020-09-29 01:00:11 +0100

    Updates to nexrad regridding

--- utils/nexrad.py
+++ utils/nexrad.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pyart
 import tarfile
+from scipy import stats
+
+from .abi import get_abi_x_y
+from .dataset import get_ds_bin_edges
 
 def get_gates_from_tar(nexrad_archive):
     time_list = []
@@ -35,3 +39,32 @@ def get_gates_from_tar(nexrad_archive):
     refs = np.concatenate(ref_list, 0)
 
     return times, alts, lats, lons, refs
+
+def map_nexrad_to_goes(nexrad_lat, nexrad_lon, nexrad_alt, goes_ds):
+    rad_x, rad_y = get_abi_x_y(nexrad_lat, nexrad_lon, goes_ds)
+    height = goes_ds.goes_imager_projection.perspective_point_height
+    lat_0 = goes_ds.goes_imager_projection.latitude_of_projection_origin
+    lon_0 = goes_ds.goes_imager_projection.longitude_of_projection_origin
+
+    dlat = np.degrees(nexrad_alt*np.tan(np.radians(nexrad_lat-lat_0) + rad_y/height)/6.371E6)
+    dlon = np.degrees(nexrad_alt*np.tan(np.radians(nexrad_lon-lon_0) + rad_x/height)/6.371E6)
+    rad_x, rad_y = get_abi_x_y(nexrad_lat+dlat, nexrad_lon+dlon, goes_ds)
+
+    return rad_x, rad_y
+
+def get_nexrad_hist(nexrad_ref, nexrad_time, nexrad_alt, nexrad_lat, nexrad_lon,
+                    goes_ds, start_time, end_time, min_alt=2500, max_alt=15000):
+
+    wh_t = np.logical_and(nexrad_time>=start_time, nexrad_time<end_time)
+    mask = np.logical_and(nexrad_alt[wh_t]>min_alt, nexrad_alt[wh_t]<max_alt)
+    x,y = map_nexrad_to_goes(nexrad_lat[wh_t][mask], nexrad_lon[wh_t][mask],
+                                    nexrad_alt[wh_t][mask], goes_ds)
+
+    x_bins, y_bins = get_ds_bin_edges(goes_ds, ('x','y'))
+    counts_hist = np.histogram2d(y, x, bins=(y_bins[::-1], x_bins))[0][::-1]
+    ref_hist = stats.binned_statistic_dd((y, x), nexrad_ref[wh_t][mask],
+                                         statistic='mean',
+                                         bins=(y_bins[::-1], x_bins),
+                                         expand_binnumbers=True)[0][::-1]
+
+    return counts_hist, ref_hist
