1ed1fa34c6bedaf4352f7ee237d92c72757803fa
William Jones <william.jones@physics.ox.ac.uk>
2020-09-29 04:34:10 +0100

    Fixes to nexrad script

--- utils/nexrad.py
+++ utils/nexrad.py
@@ -73,5 +73,5 @@ def get_nexrad_hist(nexrad_ref, nexrad_time, nexrad_alt, nexrad_lat, nexrad_lon,
 
 def get_site_grids(nexrad_file, goes_ds, goes_dates):
     radar_gates = get_gates_from_tar(nexrad_file)
-    temp_stack = [get_nexrad_hist(*radar_gates_3, goes_ds, dt, dt+timedelta(minutes=5)) for dt in goes_dates]
+    temp_stack = [get_nexrad_hist(*radar_gates, goes_ds, dt, dt+timedelta(minutes=5)) for dt in goes_dates]
     return [np.stack(temp) for temp in zip(*temp_stack)]
