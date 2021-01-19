5ce3e5a880202f2d6405c8fe3d915a743f837b93
William Jones <william.jones@physics.ox.ac.uk>
2021-01-19 09:41:43 +0000

    Try and catch OSError when creating directories

--- scripts/dcc_detect.py
+++ scripts/dcc_detect.py
@@ -43,7 +43,7 @@ save_dir = args.sd
 if not os.path.isdir(save_dir):
     try:
         os.makedirs(save_dir)
-    except FileExistsError:
+    except FileExistsError, OSError:
         pass
 
 save_name = 'detected_dccs_%s.nc' % (start_date.strftime('%Y%m%d_%H0000'))
@@ -65,7 +65,10 @@ from utils.validation import get_min_dist_for_objects, get_marker_distance
 
 goes_data_path = args.gd
 if not os.path.isdir(goes_data_path):
-    os.makedirs(goes_data_path)
+    try:
+        os.makedirs(goes_data_path)
+    except FileExistsError, OSError:
+        pass
 
 print(datetime.now(),'Loading ABI data')
 print('Saving data to:',goes_data_path)
